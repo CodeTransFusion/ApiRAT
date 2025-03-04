51c9c9f9379dbaf2a39863ffcfa28a6fb0cd4c6d
Qiu Guan Jie <1505563192@qq.com>
2025-03-04 16:18:36 +0800

    update

--- scripts/retrieve.sh
+++ scripts/retrieve.sh
@@ -6,36 +6,30 @@ export PYTHONIOENCODING=utf-8;
 
 if [ "$#" -ne 5 ]; then
   echo "Usage: $0 <source_lang> <target_lang> <dataset> <model> <gpu_id>"
-  echo "Supported models: bm25, codebert, unixcoder, graphcodebert, starencoder, text-embedding-3"
+  echo "Supported models: BM25, CodeBert, UniXCoder, GraphCodeBert, StarEncoder, Text-Embedding-3"
   exit 1
 fi
 
-SOURCE_LANG=$1
-TARGET_LANG=$2
-DATASET=$3
-MODEL=$4
+MODEL=$1
+DATASET=$2
+SOURCE_LANG=$3
+TARGET_LANG=$4
 GPU_ID=$5
 
-if [ "$MODEL" == "bm25" ]; then
+if [ "$MODEL" == "BM25" ]; then
   python3 retriever/bm25.py --source_lang "$SOURCE_LANG" --target_lang "$TARGET_LANG" --dataset "$DATASET"
 
-elif [ "$MODEL" == "codebert" ]; then
+elif [ "$MODEL" == "CodeBert" ]; then
   python3 retriever/codebert.py --source_lang "$SOURCE_LANG" --target_lang "$TARGET_LANG" --dataset "$DATASET" --gpu_id "$GPU_ID"
 
-elif [ "$MODEL" == "unixcoder" ]; then
+elif [ "$MODEL" == "UniXCoder" ]; then
   python3 retriever/unixcoder.py --source_lang "$SOURCE_LANG" --target_lang "$TARGET_LANG" --dataset "$DATASET" --gpu_id "$GPU_ID"
 
-elif [ "$MODEL" == "graphcodebert" ]; then
+elif [ "$MODEL" == "GraphCodeBert" ]; then
   python3 retriever/codebert.py --source_lang "$SOURCE_LANG" --target_lang "$TARGET_LANG" --dataset "$DATASET" --output_dir "../output/graphcodebert" --model_path "../models/graphcodebert" --gpu_id "$GPU_ID"
 
-elif [ "$MODEL" == "starencoder" ]; then
+elif [ "$MODEL" == "StarEncoder" ]; then
   python3 retriever/starencoder.py --source_lang "$SOURCE_LANG" --target_lang "$TARGET_LANG" --dataset "$DATASET" --gpu_id "$GPU_ID"
 
-elif [ "$MODEL" == "text-embedding-3" ]; then
+elif [ "$MODEL" == "Text-Embedding-3" ]; then
   python3 retriever/text-embedding-v3.py --source_lang "$SOURCE_LANG" --target_lang "$TARGET_LANG" --dataset "$DATASET"
-
-else
-  echo "Error: Unsupported model '$MODEL'."
-  echo "Supported models: bm25, codebert, unixcoder, graphcodebert, starencoder, text-embedding-3"
-  exit 1
-fi
